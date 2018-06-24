95bc9edc9f0b89e0ec9978293b4b8cba7fe05cd9
George White <george@majilis.co.uk>
2018-06-24 17:40:17 +0000

    Update dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,60 +16,6 @@ const browserSync = BrowserSync.create();
 const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
 const hugoArgsPreview = ["--buildDrafts", "--buildFuture"];
 
-// Development tasks
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
-
-// Build/production tasks
-gulp.task("build", ["css", "js", "fonts"], (cb) => buildSite(cb, [], "production"));
-gulp.task("build-preview", ["css", "js", "fonts"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
-
-// Compile CSS with PostCSS
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(browserSync.stream())
-));
-
-// Compile Javascript
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
-
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
-});
-
-// Move all fonts in a flattened directory
-gulp.task('fonts', () => (
-  gulp.src("./src/fonts/**/*")
-    .pipe(flatten())
-    .pipe(gulp.dest("./dist/fonts"))
-    .pipe(browserSync.stream())
-));
-
-// Development server with browsersync
-gulp.task("server", ["hugo", "css", "js", "fonts"], () => {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    },
-    port: process.env.PORT || 3000,
-    host: process.env.IP || "127.0.0.1"
-  });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/css/**/*.css", ["css"]);
-  gulp.watch("./src/fonts/**/*", ["fonts"]);
-  gulp.watch("./site/**/*", ["hugo"]);
-});
-
 /**
  * Run hugo and build the site
  */
@@ -88,3 +34,60 @@ function buildSite(cb, options, environment = "development") {
     }
   });
 }
+
+// Compile Javascript
+gulp.task("js", (cb) => {
+  var myConfig = Object.assign({}, webpackConfig);
+  myConfig.mode = process.env.NODE_ENV;
+
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString({
+      colors: true,
+      progress: true
+    }));
+    browserSync.reload();
+    cb();
+  });
+});
+
+// Move all fonts in a flattened directory
+gulp.task("fonts", () => (
+  gulp.src("./src/fonts/**/*")
+    .pipe(flatten())
+    .pipe(gulp.dest("./dist/fonts"))
+    .pipe(browserSync.stream())
+));
+
+// Development tasks
+gulp.task("hugo", (cb) => buildSite(cb));
+gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
+
+// Compile CSS with PostCSS
+gulp.task("css", () => (
+  gulp.src("./src/css/*.css")
+    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream())
+));
+
+// Build/production tasks
+gulp.task("build", gulp.series(["css", "js", "fonts"],
+  (cb) => buildSite(cb, [], "production")));
+gulp.task("build-preview", gulp.series(["css", "js", "fonts"],
+  (cb) => buildSite(cb, hugoArgsPreview, "production")));
+
+// Development server with browsersync
+gulp.task("server", gulp.series(["hugo", "css", "js", "fonts"], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    },
+    port: process.env.PORT || 3000,
+    host: process.env.IP || "127.0.0.1"
+  });
+  gulp.watch("./src/js/**/*.js", gulp.parallel(["js"]));
+  gulp.watch("./src/css/**/*.css", gulp.parallel(["css"]));
+  gulp.watch("./src/fonts/**/*", gulp.parallel(["fonts"]));
+  gulp.watch("./site/**/*", gulp.parallel(["hugo"]));
+}));
